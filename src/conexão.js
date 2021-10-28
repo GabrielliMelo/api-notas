758bb1f3e6387cea66d36ea1ac18ee6a326655bd
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2021-10-28 19:49:46 -0300

    API NOTAS

--- src/conexão.js
+++ src/conexão.js
@@ -5,7 +5,8 @@ const knex = require('knex')({
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
-        port: process.env.DATABASE_PORT
-    }
+        port: process.env.DATABASE_PORT,
+        ssl: process.env.DATABASE_SSL
+    },
 })
 module.exports = knex;
